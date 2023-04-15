bbf6c99b0f15a9e5e8a3d61e427f8d47a15dfa3e
diegoandrade-git <diegoaparecido2015@gmail.com>
2023-04-15 16:57:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+      
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+
+
+})
+
